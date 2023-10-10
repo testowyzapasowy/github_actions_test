9232f7fe16b4b25229fc708d2d43fc38d432b7bb
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-10-10 17:12:59 +0200

    wip

---
+++ get_ticket_id.exs
@@ -0,0 +1,14 @@
+Mix.install([:json])
+
+[issue_url] = System.argv()
+
+Process.sleep(1000)
+
+:ok =
+  IO.read(:stdio, :eof)
+  |> IO.inspect(label: "STDIN STRING", limit: :infinity)
+  |> JSON.decode!()
+  |> Map.get("items")
+  |> Enum.find(& &1["content"]["url"] == issue_url)
+  |> Map.get("id")
+  |> IO.binwrite()
