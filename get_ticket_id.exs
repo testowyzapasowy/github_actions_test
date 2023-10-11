c9664f4cfade6000dd8fa33f7119ffe41296c593
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-10-11 15:44:52 +0200

    wip

--- get_ticket_id.exs
+++ get_ticket_id.exs
@@ -4,16 +4,11 @@ Mix.install([:json])
 
 Process.sleep(1000)
 
-# {_collectable, 0} =
+:ok =
   IO.read(:stdio, :eof)
-  # |> IO.inspect(label: "STDIN STRING", limit: :infinity)
   |> JSON.decode!()
-  # |> IO.inspect(label: "PARSED JSON", pretty: true, limit: :infinity)
   |> Map.get("items")
   |> Enum.find(& &1["content"]["url"] == issue_url)
   |> Map.get("id")
   |> then(& "\n\nTICKET_ID #{&1}")
   |> IO.puts()
-  # |> IO.inspect(label: "TICKET_ID")
-  # |> then(& "export #{ticket_id_env}=\"#{&1}\"")
-  # |> System.shell(into: IO.stream())
