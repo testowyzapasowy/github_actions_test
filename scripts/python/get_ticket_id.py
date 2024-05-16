18bb4a9400cdfc47ef7a538642b5e3a10f4a2cb3
Bartek Chaliński <b.chalinski@gmail.com>
2024-05-16 02:39:39 +0200

    python fix

--- scripts/python/get_ticket_id.py
+++ scripts/python/get_ticket_id.py
@@ -1,6 +1,6 @@
 import sys, json;
 
-# file = open('generated.json')
+# file = open("generated.json")
 # full_json = json.load(file)
 # file.close()
 
@@ -8,15 +8,16 @@ full_json = json.load(sys.stdin)
 
 pr_url = sys.argv[1]
 
-try:
-    project_items = full_json["items"]
-    [id] = [item["id"] for item in project_items if ("url" in item["content"] and item["content"]["url"] == pr_url)]
-    print(id)
-except:
-    print("An exception occurred in get_ticket.py")
-    print("provided json")
-    print(full_json)
-    # print("provided PR_URL:", pr_url)
+project_items = full_json["items"]
+
+item_id = None
+for item in project_items:
+    if "content" in item and "url" in item["content"]:
+        if item["content"]["url"] == pr_url:
+            item_id = item["id"]
+            break
+print(item_id)
+
+if item_id == None:
+    print("An exception occurred in get_ticket.py (item_id == None)")
     sys.exit(1)
-
-
